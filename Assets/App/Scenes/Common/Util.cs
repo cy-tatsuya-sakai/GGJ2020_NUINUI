5cf9e22ae8bf570f1462da1e8191dfc32611ed91
tatsuya_sakai <tatsuya_sakai@cybird.co.jp>
2020-02-01 07:15:55 +0900

    マウスのワールド座標を取得する関数を追加

---
+++ Assets/App/Scenes/Common/Util.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// とりあえず突っ込む場所
+/// </summary>
+public static class Util
+{
+    /// <summary>
+    /// マウス位置をカメラ座標に変更。Z値はゼロなので、いい感じに変えてね
+    /// </summary>
+    /// <param name="cam"></param>
+    /// <returns></returns>
+    public static Vector3 GetMouseWorldPosition(Camera cam)
+    {
+        Vector3 pos = Input.mousePosition;
+        pos.z = -cam.transform.position.z;
+        return cam.ScreenToWorldPoint(pos);
+    }
+}
