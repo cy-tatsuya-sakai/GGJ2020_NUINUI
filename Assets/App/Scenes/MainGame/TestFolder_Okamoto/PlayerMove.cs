df9e6b884700cfb6c19ac40c5d92c11dc64eaec4
m0117067d4@edu.teu.ac.jp <50638616+Rayos09@users.noreply.github.com>
2020-02-01 14:00:43 +0900

    PlayerとMarkerのプレハブ化

--- Assets/App/Scenes/MainGame/TestFolder_Okamoto/PlayerMove.cs
+++ Assets/App/Scenes/MainGame/TestFolder_Okamoto/PlayerMove.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    Camera cam;
     public Transform Marker;
-    public GameObject _markingPinPrefab;
+    private float distance_two;
+    public float speed = 1.0F;
 
     // Start is called before the first frame update
     void Start()
@@ -17,14 +19,12 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         transform.LookAt(Marker);
-        
-    }
-
-   
-
-    public void Move()
-    {
-        Vector3 Pinpos = _markingPinPrefab.transform.position;
-        gameObject.transform.position = Pinpos;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = gameObject.transform.position;
+            distance_two = Vector3.Distance(pos, Marker.position);
+            float present_Location = (Time.time * speed) / distance_two;
+            transform.position = Vector3.Lerp(pos, Marker.position, present_Location);
+        }
     }
 }
---
+++ Assets/App/Scenes/MainGame/TestFolder_Sakai/EnemyCollision.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 敵とか穴とか、識別するためのスクリプト
+/// </summary>
+public class EnemyCollision : MonoBehaviour
+{
+
+}
