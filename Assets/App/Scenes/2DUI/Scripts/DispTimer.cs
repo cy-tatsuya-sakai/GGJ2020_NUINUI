2d70674fc3058db4d07ad7aaf7cac83428df8595
G-TUNENEXTGEAR-\takoi <tako.ishihata@gmail.com>
2020-02-01 16:00:54 +0900

    game timer modified.

---
+++ Assets/App/Scenes/2DUI/Scripts/DispTimer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DispTimer : MonoBehaviour
+{
+    public Text text;
+
+    GameObject objGameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objGameManager = GameObject.Find("GameManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float counter = objGameManager.GetComponent<GameStateContoller>().GetGameCounter();
+        text.text = "Text";
+    }
+}
