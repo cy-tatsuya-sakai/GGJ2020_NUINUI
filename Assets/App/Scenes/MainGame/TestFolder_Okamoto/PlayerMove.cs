948a31d707f7dabdc6bf00d5d138d3970c2152e4
m0117067d4@edu.teu.ac.jp <50638616+Rayos09@users.noreply.github.com>
2020-02-01 11:28:34 +0900

    プレイヤーがマーカーの方向を向くようにしました

---
+++ Assets/App/Scenes/MainGame/TestFolder_Okamoto/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Transform Marker;
+    public GameObject _markingPinPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Marker);
+        
+    }
+
+   
+
+    public void Move()
+    {
+        Vector3 Pinpos = _markingPinPrefab.transform.position;
+        gameObject.transform.position = Pinpos;
+    }
+}
