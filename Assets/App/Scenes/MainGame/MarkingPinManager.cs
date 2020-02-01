7de89c7dd80b0a81ab68bdd6889a01cd40b86336
tatsuya_sakai <tatsuya_sakai@cybird.co.jp>
2020-02-01 09:12:31 +0900

    マチ針打つのと、三角形の判定だけ追加

---
+++ Assets/App/Scenes/MainGame/MarkingPinManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+/// <summary>
+/// なんかマチ針を保持。三角形を作るためのリスト。その他計算
+/// </summary>
+public class MarkingPinManager : MonoBehaviour
+{
+    [SerializeField] private MarkingPin _markingPinPrefab;
+    [SerializeField] private Patchwork  _patchworkPrefab;
+
+    private List<MarkingPin>    _markingPinList = new List<MarkingPin>();
+    private List<Patchwork>     _patchworkList = new List<Patchwork>();
+
+    public void Init()
+    {
+        _markingPinList.Clear();    // 意味ない
+        _patchworkList.Clear();
+    }
+
+    /// <summary>
+    /// 針を追加
+    /// </summary>
+    /// <param name="pos"></param>
+    public void AddPin(Vector3 pos)
+    {
+        var obj = Instantiate(_markingPinPrefab, pos, Quaternion.identity);
+        obj.transform.SetParent(transform);
+        _markingPinList.Add(obj);
+        obj.Init(_markingPinList.Count - 1);
+    }
+
+    /// <summary>
+    /// 三角形を作る
+    /// </summary>
+    public void CreatePatchwork()
+    {
+        int num = _markingPinList.Count;
+        if(num < 4) { return; }
+        var a = _markingPinList[num - 1].transform.position;
+        var b = _markingPinList[num - 2].transform.position;
+        var c = _markingPinList[num - 3].transform.position;
+        var d = _markingPinList[num - 4].transform.position;
+
+        
+        var ad = (a - d).sqrMagnitude;  // 1点目と4点目が十分近いならきっと三角形
+        if(ad <= 0.001f)
+        {
+            var ab = a - b;
+            var ac = a - c;
+            var nn = new Vector3(-ab.y, ab.x, ab.z).normalized;
+            if(Mathf.Abs(Vector3.Dot(nn, ac)) > 0.001f)
+            {
+                // 多少高さもあるみたいだし、多分三角形…
+                _markingPinList.Clear();
+                CreatePatchwork_(a, b, c);
+                Debug.Log("YYYYYYYYYYYYYYYYYYYYYY");
+            }
+        }
+        else
+        {
+            Debug.Log("BBBBBBBBBBBBBBBBBBBBBBB");
+        }
+    }
+
+    private void CreatePatchwork_(Vector3 a, Vector3 b, Vector3 c)
+    {
+        
+    }
+
+    /// <summary>
+    /// クリックした位置のマチ針があれば返す。なければnull
+    /// </summary>
+    /// <param name="pos"></param>
+    /// <returns></returns>
+    public MarkingPin GetMarkingPin(Vector3 pos)
+    {
+        var collList = Physics2D.OverlapPointAll(new Vector2(pos.x, pos.y));
+        Debug.Log(collList.Length);
+        int idx = -1;
+        foreach(var coll in collList)
+        {
+            var pin = coll.gameObject.GetComponentInParent<MarkingPin>();
+            if(pin == null) { continue; }
+
+            if(pin.Index > idx)
+            {
+                idx = pin.Index;
+            }
+        }
+
+        if(idx < 0 || idx >= _markingPinList.Count)
+        {
+            return null;
+        }
+
+        return _markingPinList[idx];
+    }
+}
---
+++ Assets/App/Scenes/MainGame/TestFolder_Sakai/MarkingPinTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkingPinTest : MonoBehaviour
+{
+    [SerializeField] private Camera _cam;
+    [SerializeField] private GameObject testObj;
+    [SerializeField] private MarkingPinManager _markingPinManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        testObj.transform.position = Util.GetMouseWorldPosition(_cam);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            var pin = _markingPinManager.GetMarkingPin(testObj.transform.position);
+            var pos = (pin == null) ? testObj.transform.position : pin.transform.position;
+            _markingPinManager.AddPin(pos);
+
+            _markingPinManager.CreatePatchwork();
+        }
+    }
+}
