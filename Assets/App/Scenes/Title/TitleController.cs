9b2c2ad64dacbcf228c5a7933b8be5307ea3e8e9
tatsuya_sakai <tatsuya_sakai@cybird.co.jp>
2020-02-02 08:22:20 +0900

    タイトルBGMを再生

--- Assets/App/Scenes/Title/TitleController.cs
+++ Assets/App/Scenes/Title/TitleController.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Lib.Sound;
 
 public class TitleController : MonoBehaviour
 {
     void Start()
     {
-        
+        SoundManager.Instance.BGM.PlayCrossFade(_BGM._GAME_MUSIC);
     }
 
     void Update()
--- Assets/Lib/Sound/Prefabs/_BGM.cs
+++ Assets/Lib/Sound/Prefabs/_BGM.cs
@@ -1,5 +1,6 @@
 public static class _BGM
 {
     public const string _A = "A";
+    public const string _GAME_MUSIC = "GAME_MUSIC";
     public const string _TITLE_BGM = "TITLE_BGM";
 }
