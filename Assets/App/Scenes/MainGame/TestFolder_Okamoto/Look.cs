d70931ec986b7c83fccf49918f397d5b98c64fe9
m0117067d4@edu.teu.ac.jp <50638616+Rayos09@users.noreply.github.com>
2020-02-02 02:53:37 +0900

    プレイヤーが滑らかに動くようになりました

---
+++ Assets/App/Scenes/MainGame/TestFolder_Okamoto/Look.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour
+{
+
+    public Transform Marker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.LookAt(Marker);
+            Debug.Log("look");
+        }
+    }
+}
